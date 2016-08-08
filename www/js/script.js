faee131bbcb07b8b0586e6fa2418887e1254bc9e
Craig Wells <craig@vyreadminsair2.home>
2016-08-08 23:28:29 +0100

    custom directive and Font list

---
+++ www/js/script.js
@@ -0,0 +1,50 @@
+﻿(function(angular) {
+  'use strict';
+var app = angular.module('beanApp', []);
+
+app.controller('MainCtrl', function($scope, fontService){
+    $scope.fontDetails = fontService;
+    console.log($scope.fontDetails);
+});
+
+app.service('fontService', function(){
+
+	var fontList = [
+    		"Impact",
+			"Palatino Linotype",
+			"Tahoma",
+			"Century Gothic",
+			"Lucida Sans Unicode",
+			"Arial Black",
+			"Times New Roman",
+			"Arial Narrow",
+			"Verdana",
+			"Copperplate",
+			"Copperplate Gothic Light",
+			"Lucida Console",
+			"Gill Sans",
+			"Trebuchet MS",
+			"Courier-New",
+			"Arial",
+			"Constantina"
+	];
+
+    return {
+    	fontlist : function(){
+    		return fontList;
+    	},
+    	getClass : function(val){
+    		var temp = val.split(" ").join("-"); 
+    		return temp;
+    	}
+    }
+});
+
+app.directive('fonts', function(){
+	return {
+		restrict: 'E',
+		templateUrl: 'templates/fonts.html'
+	}
+});
+
+})(window.angular);
